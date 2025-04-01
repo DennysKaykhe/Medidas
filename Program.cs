248d544bd2e9b4ed6f36e783d8dcf4796ef7e1cd
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-01 19:42:46 -0300

    Eu amo converter medidas

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿double cm;
+double m;
+double km;
+
+Console.Clear();
+
+Console.Write("Entre com a medida (em metros): ");
+m = Convert.ToDouble (Console.ReadLine());
+
+Console.WriteLine(@"
+--- Equivalência ---");
+
+cm = m / 100;
+
+Console.WriteLine($"{cm} cm");
+
+Console.WriteLine($"{m} m");
+
+km = m / 1000;
+
+Console.WriteLine($"{km} km");
